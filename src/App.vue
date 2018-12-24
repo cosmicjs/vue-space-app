29b8e8daa7966e78491c8d38176ba376b6bd542e
Abey Joy <abey.joy@nyu.edu>
2018-12-23 18:51:02 -0800

    Add first component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-
+    <homescreen></homescreen>
   </div>
 </template>
 
 <script>
+import Homescreen from './components/Homescreen.vue'
 export default {
   name: 'app',
+  components: {
+    Homescreen
+  },
   data () {
     return {}
   }
